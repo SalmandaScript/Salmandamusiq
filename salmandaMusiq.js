dbd720d1e91adc3dac312c77983a95f8d1a942b8
SalmandaScript <110246267+SalmandaScript@users.noreply.github.com>
2024-07-11 15:52:49 +0200

    Add files via upload

---
+++ salmandaMusiq.js
@@ -0,0 +1,226 @@
+//https://audiomack.com/clementjumacmm/album/nice-time-the-mixtape-1/");
+
+function home(){
+open("https://salmandascript.github.io/AfricaOnline/");
+}
+
+function linkWikipedia(){
+open("https://salmandascript.github.io/LinkWikipedia-/");
+}
+
+function seer(){
+open("https://salmandascript.github.io/Seer/");
+}
+
+function africaOnline(){
+open("https://salmandascript.github.io/AfricaOnline/");
+}
+
+function royalFM(){
+open("https://salmandascript.github.io/RoyalFM/");
+}
+
+function programming(){
+open("https://salmandascript.github.io/SoftwareService/");
+}
+
+function salmandaMusiq(){
+open("https://salmandascript.github.io/Salmandamusiq/");
+}
+
+function newsChannel(){
+open("https://salmandascript.github.io/NewsChannel1/");
+}
+
+function movieChannel(){
+open("https://salmandascript.github.io/MovieChannel/");
+}
+
+/*function to change button behaviour main menu */
+
+function salmanda1(){
+document.getElementById ("demo1"). style. color ="white";
+
+document.getElementById ("demo1"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo1"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo1"). style. background ="maroon";
+}
+
+function salmanda2(){
+document.getElementById ("demo2"). style. color ="white";
+
+document.getElementById ("demo2"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo2"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo2"). style. background ="maroon";
+}
+
+function salmanda3(){
+document.getElementById ("demo3"). style. color ="white";
+
+document.getElementById ("demo3"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo3"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo3"). style. background ="maroon";
+}
+
+function salmanda4(){
+document.getElementById ("demo4"). style. color ="white";
+
+document.getElementById ("demo4"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo4"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo4"). style. background ="maroon";
+}
+
+/*function to change button behaviour main menu */
+
+function salmanda11(){
+document.getElementById ("demo11"). style. color ="white";
+
+document.getElementById ("demo11"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo11"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo11"). style. background ="maroon";
+}
+
+function salmanda22(){
+document.getElementById ("demo22"). style. color ="white";
+
+document.getElementById ("demo22"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo22"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo22"). style. background ="maroon";
+}
+
+function salmanda33(){
+document.getElementById ("demo33"). style. color ="white";
+
+document.getElementById ("demo33"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo33"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo33"). style. background ="maroon";
+}
+
+
+function salmanda44(){
+document.getElementById ("demo44"). style. color ="white";
+
+document.getElementById ("demo44"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo44"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo44"). style. background ="maroon";
+}
+
+
+function salmanda5(){
+document.getElementById ("demo5"). style. color ="white";
+
+document.getElementById ("demo5"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo5"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo5"). style. background ="maroon";
+}
+
+
+function salmanda6(){
+document.getElementById ("demo6"). style. color ="white";
+
+document.getElementById ("demo6"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo6"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo6"). style. background ="maroon";
+}
+
+function salmanda7(){
+document.getElementById ("demo7"). style. color ="white";
+
+document.getElementById ("demo7"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo7"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo7"). style. background ="maroon";
+}
+
+function salmanda8(){
+document.getElementById ("demo8"). style. color ="white";
+
+document.getElementById ("demo8"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo8"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo8"). style. background ="maroon";
+}
+
+
+function salmanda9(){
+document.getElementById ("demo9"). style. color ="white";
+
+document.getElementById ("demo9"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo9"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo9"). style. background ="maroon";
+}
+
+
+function salmanda10(){
+document.getElementById ("demo10"). style. color ="white";
+
+document.getElementById ("demo10"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo10"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo10"). style. background ="maroon";
+}
+
+function salmanda31(){
+document.getElementById ("demo31"). style. color ="white";
+
+document.getElementById ("demo31"). style. fontweight ="bold";
+
+document.getElementById
+ ("demo31"). style. fontsize ="30%";
+
+document.getElementById
+ ("demo31"). style. background ="maroon";
+}
